c7c31314cef2f3bd6c9f8f98c608e60fd2c7931d
Kbuczynski <kamil.buczynski00@interia.pl>
2020-11-09 00:40:25 +0100

    add arrows to navigation

--- src/components/Photos/PhotosSimilar/index.js
+++ src/components/Photos/PhotosSimilar/index.js
@@ -1,10 +1,13 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { API_WORDS } from "../../../constants";
 
 const PhotosSimilar = ({ inputValue }) => {
   const [result, setResult] = useState({});
+  const [scrollValue, setScrollValue] = useState(0);
+  const [prevScrollValue, setPrevScrollValue] = useState(0);
+  const containerRef = useRef();
 
   useEffect(() => {
     const getSuggestions = () => {
@@ -24,9 +27,34 @@ const PhotosSimilar = ({ inputValue }) => {
     };
   }, [inputValue]);
 
+  const handleRight = () => {
+    containerRef.current.scroll(scrollValue + 100, 0);
+    setPrevScrollValue(containerRef.current.scrollLeft);
+  };
+
+  const handleLeft = () => {
+    containerRef.current.scroll(scrollValue - 100, 0);
+  };
+
+  const handleScroll = () => {
+    setScrollValue(containerRef.current.scrollLeft);
+  };
+
   return (
     <div className="photos__similar">
-      <div className="photos__similar__container">
+      <div
+        className={`photos__similar__leftArrow ${
+          scrollValue === 0 && "unactive"
+        }`}
+        onClick={handleLeft}
+      >
+        <i className="icon-left"></i>
+      </div>
+      <div
+        className="photos__similar__container"
+        ref={containerRef}
+        onScroll={handleScroll}
+      >
         {Object.keys(result).length > 0 && result.data.length > 0
           ? result.data.map(({ word }) => (
               <Link
@@ -38,6 +66,14 @@ const PhotosSimilar = ({ inputValue }) => {
               </Link>
             ))
           : null}
+      </div>
+      <div
+        className={`photos__similar__rightArrow ${
+          scrollValue === prevScrollValue && prevScrollValue !== 0 && "unactive"
+        }`}
+        onClick={handleRight}
+      >
+        <i className="icon-right"></i>
       </div>
     </div>
   );
